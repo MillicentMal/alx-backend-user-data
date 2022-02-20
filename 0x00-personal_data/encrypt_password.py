9869fbc16b90648536cf16f9f6b05a61bb5eec48
MillicentMal <71253520+MillicentMal@users.noreply.github.com>
2022-02-20 03:16:56 +0200

    pep8 errors

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,12 +4,13 @@ Password hashing
 """
 
 import bcrypt
+
+
 def hash_password(password: str) -> bytes:
-    """ 
+    """
     Hshing passwords
     """
-    
+
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
     return hashed
-
