f5fbec01df73d56d34714c82ff5b57fd4d2d2c64
MillicentMal <71253520+MillicentMal@users.noreply.github.com>
2022-02-20 02:26:32 +0200

    fixing error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -46,18 +46,13 @@ def before_request() ->  str:
     define before_request method
     """
     excluded_paths = ['/api/v1/status', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
-    elif auth is None:
+    if not auth or not auth.require_auth(request.path, excluded_paths):
         return 
-    elif auth.require_auth(request.path, excluded_paths) is None:
-        return
-    
-         
-
-
+    elif auth.authorization_header(request) is None:
+        abort(401)
+    elif auth.current_user(request) is None:
+        abort(403)
+ 
 
 
 if __name__ == "__main__":
